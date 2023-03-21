ab167dac935457da5df7d3105db77f7c61316f4b
Bogdan Radu <bradu@adobe.com>
2023-03-21 19:30:11 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,35 @@
+import websocket
+import _thread
+import time
+import rel
+
+def on_message(ws, message):
+    print(message)
+    ws.send("Hello, World")
+    # print("Sent")
+    # print("Receiving...")
+    # result =  ws.
+    # print("Received '%s'" % result)
+    time.sleep(1)
+
+def on_error(ws, error):
+    print(error)
+
+def on_close(ws, close_status_code, close_msg):
+    print("### closed ###")
+
+def on_open(ws):
+    print("Opened connection")
+    ws.send("Hello, World")
+
+if __name__ == "__main__":
+    # websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://localhost:1234/",
+                              on_open=on_open,
+                              on_message=on_message,
+                              on_error=on_error,
+                              on_close=on_close)
+
+    ws.run_forever(dispatcher=rel, reconnect=5)  # Set dispatcher to automatic reconnection, 5 second reconnect delay if connection closed unexpectedly
+    rel.signal(2, rel.abort)  # Keyboard Interrupt
+    rel.dispatch()
